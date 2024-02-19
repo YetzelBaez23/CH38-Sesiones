6d94a9ecf8e1ba531e1edcd0754ca0637661f748
Yetzel Baez <yetzelc.baez@gmail.com>
2024-02-19 13:30:32 -0600

    added javas

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hola Ch38");
+function saludo(quien){ alert(`Saludos ${quien}`);}saludo("ch38");
+console.log("Hola Ch38");
+function SaludarAlaBanda() {
+    let nombre = prompt("Hola, ¿cómo te llamas?");
+    if (nombre) {
+        alert("¡Hola, " + nombre + "! Bienvenido.");
+    } else {
+        alert("No se introdujo ningún nombre. ¡Hola, desconocido!");
+    }
+}
+SaludarAlaBanda();
